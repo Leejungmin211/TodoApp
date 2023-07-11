35c53192790d67945007aaca5727687f8922e819
Leejungmin211 <jungmin910211@gmail.com>
2023-07-11 22:01:08 +0900

    feat: 레이아웃 및 css 추가

--- src/components/Header/TopHeader.jsx
+++ src/components/Header/TopHeader.jsx
@@ -1,14 +1,30 @@
 import { Link } from "react-router-dom";
 import styles from "./TopHeader.module.css";
+import { ReactComponent as Logo } from "../../images/logo.svg";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 export default function TopHeader() {
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <header className={styles.header}>
-      <Link to="/">DAILY PLANNER</Link>
+      <Link to="/">
+        <Logo className={styles.logo} />
+      </Link>
       <div>
-        <Link to="/">Todo</Link>
-        <Link to="/mypage">Mypage</Link>
+        <Link to="/" className={styles.list}>
+          CALENDAR
+        </Link>
+        <Link to="/" className={styles.list}>
+          TODO LIST
+        </Link>
+        <Link to="/mypage" className={styles.list}>
+          ABOUT ME
+        </Link>
       </div>
+      <button className={styles.button} onClick={toggleDarkMode}>
+        {!darkMode && "다크모드"}
+        {darkMode && "라이트모드"}
+      </button>
     </header>
   );
 }
