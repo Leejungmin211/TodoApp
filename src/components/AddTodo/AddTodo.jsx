c0ed4119eead542f863656781641aee827efc39d
Leejungmin211 <jungmin910211@gmail.com>
2023-07-13 22:38:12 +0900

    feat: todo 수정버튼, 다크모드 색상 추가

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import styles from "./AddTodo.module.css";
 
 export default function AddTodo({ handleAdd }) {
   const [text, setText] = useState("");
@@ -14,14 +15,17 @@ export default function AddTodo({ handleAdd }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <input
+        className={styles.input}
         type="text"
         placeholder="Add Todo"
         value={text}
         onChange={handleText}
       ></input>
-      <button type="submit">Add</button>
+      <button className={styles.button} type="submit">
+        Add
+      </button>
     </form>
   );
 }
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "./Todo.module.css";
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { id, text, status } = todo;
+  const [isEdit, setIsEdit] = useState(false);
+  const [editText, setEditText] = useState(text);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (isEdit) {
+      inputRef.current.focus();
+    }
+  }, [isEdit]);
+
+  useEffect(() => {
+    setEditText(text);
+  }, [text]);
+
   const handleChecked = (e) => {
     const status = e.target.checked ? "Completed" : "Active";
     onUpdate({ ...todo, status });
   };
   const handleDelete = () => {
     onDelete(todo);
+  };
+  const handelEdit = () => {
+    setIsEdit(true);
+  };
+  const handleEditText = (e) => {
+    setEditText(e.target.value);
+  };
+  const handleSave = () => {
+    onUpdate({ ...todo, text: editText });
+    setIsEdit(false);
   };
 
   return (
@@ -19,12 +44,33 @@ export default function Todo({ todo, onUpdate, onDelete }) {
         checked={status === "Completed"}
         onChange={handleChecked}
       />
-      <label className={styles.label} htmlFor={id}>
-        {text}
-      </label>
+      {isEdit ? (
+        <input
+          className={styles.edit}
+          type="text"
+          id={id}
+          value={editText}
+          onChange={handleEditText}
+          ref={inputRef}
+        />
+      ) : (
+        <label className={styles.label} htmlFor={id}>
+          {text}
+        </label>
+      )}
       <div>
-        <button onClick={handleDelete}>수정</button>
-        <button onClick={handleDelete}>삭제</button>
+        {isEdit ? (
+          <button className={styles.button} onClick={handleSave}>
+            저장
+          </button>
+        ) : (
+          <button className={styles.button} onClick={handelEdit}>
+            수정
+          </button>
+        )}
+        <button className={styles.button} onClick={handleDelete}>
+          삭제
+        </button>
       </div>
     </li>
   );
